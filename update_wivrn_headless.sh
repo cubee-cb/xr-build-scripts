a82f0af654893e63aceea21afbbc48160dd3aa8e
cubee <cubeecb+github@gmail.com>
2026-03-19 10:23:14 +1100

    fix headless stopping server even when there are no changes

--- update_wivrn_headless.sh
+++ update_wivrn_headless.sh
@@ -5,9 +5,9 @@ echo "      Updating WiVRn Server (no Dashboard)"
 echo "==============================================="
 
 cd wivrn
-systemctl stop --user wivrn-git
 git fetch
 if [[ `git rev-list HEAD...origin/master --count` > 0 ]]; then
+  systemctl stop --user wivrn-git
   git pull
   cmake -B build-server . -GNinja -Wno-dev -DWIVRN_BUILD_CLIENT=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWIVRN_USE_PIPEWIRE=ON
   cmake --build build-server
